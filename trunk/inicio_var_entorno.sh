af0ed144a9944891dd0ffef8920f4ed6079d6a5d
juanfedericofuld@hotmail.com <juanfedericofuld@hotmail.com@7dd76c45-ab5f-dfa8-2945-61286fd544e2>
2014-04-26 23:11:52 +0000

    Hice el listener y cambie el inicio var de entorno para que cree una estructura de direcorios o se me complicaba mover cosas y masterlist y rating no tienen nada adentro, pero los necesitaba para ver si los estaba llamando o no

--- trunk/inicio_var_entorno.sh
+++ trunk/inicio_var_entorno.sh
@@ -1,6 +1,44 @@
 #!/bin/sh
 export INIT=true
-export LOGDIR=.
-export BINDIR=.
+
+if [ ! -d ./logs ]
+then
+	mkdir logs
+fi
+
+if [ ! -d ./binarios ]
+then
+	mkdir binarios
+fi
+
+if [ ! -d ./maestros ]
+then
+	mkdir maestros
+	cd maestros
+	mkdir precios
+	cd ..
+fi
+
+if [ ! -d ./aceptados ]
+then
+	mkdir aceptados
+fi
+
+if [ ! -d ./novedades ]
+then
+	mkdir novedades
+fi
+
+if [ ! -d ./rechazados ]
+then
+	mkdir rechazados
+fi
+
+export LOGDIR=./logs
+export BINDIR=./binarios
 export LOGEXT=log
 export LOGSIZE=1
+export MAEDIR=./maestros
+export ACEPDIR=./aceptados
+export NOVEDIR=./novedades
+export RECHDIR=./rechazados
