113af7b8521695b652eabdc6822d6c49dba5107c
ramirodoi <ramirodoi@7dd76c45-ab5f-dfa8-2945-61286fd544e2>
2014-04-29 18:31:31 +0000

    + Se agregó el menu para el reporting.pl + Se agregó la funcionalidad de guarda (-w) * Se actualizaron varias cosas

--- trunk/reporting.pl
+++ trunk/reporting.pl
@@ -1,59 +1,171 @@
 #! /usr/bin/perl
 
-open(LISTA_PRESUPUESTADA,"<pepe.xxx") || die "ERROR: el archivo de Lista Presupuestada no existe";
-open(SUPER, "SUPER.MAE") || die "ERROR: el archivo de SUPER.MAE no existe";
+# ------------
+# INICIO MAIN.
+# ------------
+system("clear");
 
-# --------------------------------
-# Cargamos la Lista Presupuestada.
-# --------------------------------
-$cantDeLineas = 0;
-%hashListaPresupuestada = ($cantDeLineas, "Informacion");
+$opcion = "";
 
-while ($lineaEntrada = <LISTA_PRESUPUESTADA>)
+while ($opcion ne "-exit")
 {
-	$cantDeLineas++;
-	$hashListaPresupuestada{$cantDeLineas} = $lineaEntrada;
+	print "\n\n";
+	print "----------------------------------------------------------------------------------\n";
+	print "1ra entrada --> Opcion de proceso ('-a' para mas informacion | '-exit' para salir)\n";
+	print "2da entrada --> Path del archivo de Lista Presupuestada\n";
+	print "3ra entrada --> Opcion de grabado ('-w') [y/n]\n";
+	print "----------------------------------------------------------------------------------\n";
+
+	print "1ra entrada: ";
+	$opcion = <STDIN>;
+	chomp($opcion);
+
+	if ($opcion ne "-exit" and  $opcion ne "-a" and $opcion ne "-w" and $opcion ne "-r" and $opcion ne "-m" and $opcion ne "-d" and $opcion ne "-f" and $opcion ne "-x" and $opcion ne "-u" and $opcion ne "-mr" and $opcion ne "-dr")
+	{
+		print "No existe comando '$opcion'. Por favor ingrese '-a' para Ayuda.\n";
+		$opcion = "-exit";
+	}
+
+	if ($opcion eq "-a")
+	{
+		&MostrarMenuA_Ayuda;
+	}
+	
+	if ($opcion eq "-r" or $opcion eq "-m" or $opcion eq "-d" or $opcion eq "-f" or $opcion eq "-x" or $opcion eq "-u" or $opcion eq "-mr" or $opcion eq "-dr")
+	{
+		print "2da entrada: ";
+		$pathListaPresupuestada = <STDIN>;
+		chomp($pathListaPresupuestada);
+
+		print "3ra entrada [y/n]: ";
+		$opcionDeGrabado = <STDIN>;
+		chomp($opcionDeGrabado);
+	
+		open(LISTA_PRESUPUESTADA,$pathListaPresupuestada) || die "ERROR: el archivo de Lista Presupuestada no existe";
+		open(SUPER, "SUPER.MAE") || die "ERROR: el archivo de SUPER.MAE no existe";
+	
+		# --------------------------------
+		# Cargamos la Lista Presupuestada.
+		# --------------------------------
+		$cantDeLineas = 0;
+		%hashListaPresupuestada = ($cantDeLineas, "Informacion");
+		
+		while ($lineaEntrada = <LISTA_PRESUPUESTADA>)
+		{
+			$cantDeLineas++;
+			$hashListaPresupuestada{$cantDeLineas} = $lineaEntrada;
+		}
+		
+		# -----------------------------------
+		# Cargamos la Lista de Supermercados.
+		# -----------------------------------
+		%hashSuper = (0, "Informacion", 1, "Precios Cuidados");
+		
+		while ($lineaEntrada = <SUPER>)
+		{
+			@arrayData = split(";", $lineaEntrada);
+			$id = $arrayData[0];
+			$nombre_provincia = $arrayData[2]."-".$arrayData[1];
+			$hashSuper{$id} = $nombre_provincia;
+		}	
+		
+		# ----------------------------------------
+		# Segun la opcion ingresa muestro la info.
+		# ----------------------------------------
+		if ($opcion eq "-r")
+		{
+			&MostrarMenuR_Referencia(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas, $opcionDeGrabado);
+		}
+	
+		if ($opcion eq "-m")
+		{
+			&MostrarMenuM_MenorPrecio(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas, $opcionDeGrabado);
+		} 
+
+		if ($opcion eq "-d")
+		{
+			print "proximamente\n";
+		}
+	
+		if ($opcion eq "-f")
+		{
+			&MostrarMenuF_Faltante(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas, $opcionDeGrabado);
+		}
+
+		if ($opcion eq "-x")
+		{
+			print "proximamente\n";
+		}
+
+		if ($opcion eq "-u")
+		{
+			print "proximamente\n";
+		}
+
+		if ($opcion eq "-mr")
+		{
+			print "proximamente\n";
+		}
+		
+		if ($opcion eq "-dr")
+		{
+			print "proximamente\n";
+		}
+		
+		close (LISTA_PRESUPUESTADA);
+		close (SUPER);
+	}
 }
 
-# -----------------------------------
-# Cargamos la Lista de Supermercados.
-# -----------------------------------
-%hashSuper = (0, "Informacion", 1, "Precios Cuidados");
+system ("clear");
+# ---------
+# FIN MAIN.
+# ---------
 
-while ($lineaEntrada = <SUPER>)
-{
-	@arrayData = split(";", $lineaEntrada);
-	$id = $arrayData[0];
-	$nombre_provincia = $arrayData[2]."-".$arrayData[1];
-	$hashSuper{$id} = $nombre_provincia;
-}
-
-
-# print "$hashListaPresupuestada{0}\n";
-# print "$hashListaPresupuestada{1}\n";
-# &MostrarMenuA_Ayuda;
-# &MostrarMenuF_Faltante(\%hashListaPresupuestada, \$hashSuper, $cantDeLineas);
-# &MostrarMenuR_Referencia(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas);
-&MostrarMenuM_MenorPrecio(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas);
-
-close (LISTA_PRESUPUESTADA);
-close (SUPER);
-
+# ------------------------------
+# SUB, Muestra el Menu de Ayuda.
+# ------------------------------
 sub MostrarMenuA_Ayuda
 {
-	print "-a --> Ayuda\n";
-	print "-w --> Grabar\n";
-	print "-r --> Precio de referencia\n";
-	print "-m --> Menor precio\n";
-	print "-d --> Donde Comprar\n";
-	print "-f --> Faltante\n";
-	print "-x --> Filtrar por Provincia/Supermercado\n";
-	print "-u --> Filtrar por Usuario\n";
-	print "Otros --> Proximamente\n";
+	system("clear");
+
+	print "----------------------------------------------------------------------------------------\n";
+	print "1ra entrada:\n";
+	print "		'-a' --> Ayuda.\n";
+	print "		'-r' --> Precio de referencia (Precios Cuidados).\n";
+	print "		'-m' --> Menor precio.\n";
+	print "		'-d' --> Donde Comprar.\n";
+	print "		'-f' --> Faltante.\n";
+	print "		'-x' --> Filtrar por Provincia/Supermercado.\n";
+	print "		'-u' --> Filtrar por Usuario.\n";
+	print "		'-exit' --> Salir.\n";
+	print "2da entrada:\n";
+	print "		Path del archivo 'Lista Presupuestada' a procesar.\n";
+	print "3ra entrada:\n";
+	print "		'-w' [y/n] --> Opcion de Grabado\n";
+	print "			       [y] no muestra por pantalla y graba en archivo de salida.\n";
+	print "			       [n] muestra por pantalla.\n";
+	print "----------------------------------------------------------------------------------------\n";
 }
 
+# -----------------------------------------------------------------
+# SUB, Muestra la info de precios de Referencia (Precios Cuidados).
+# -----------------------------------------------------------------
 sub MostrarMenuR_Referencia
 {
+	system("clear");
+
+	if ($_[3] eq "y")
+	{
+		# open(SALIDA, ">$salida.txt") || die "ERROR: El archivo 'salida.txt' no existe";
+		open(SALIDA, ">salida.txt");
+		print SALIDA "NRO de ITEM \| PRODUCTO PEDIDO \| PRODUCTO ENCONTRADO \| PRECIO \| NOMBRE_SUPER-PROVINCIA\n";
+	}
+	else
+	{
+		print "NRO de ITEM \| PRODUCTO PEDIDO \| PRODUCTO ENCONTRADO \| PRECIO \| NOMBRE_SUPER-PROVINCIA\n";
+	}
+
 	for (my $i=1;$i<=$_[2];$i++)
 	{
 		my $linea = $_[0]{$i};
@@ -62,18 +174,47 @@ sub MostrarMenuR_Referencia
 		if ($linea =~ /[^;]*;[^;]*;1;.*/)
 		{
 			my @arrayData = split(";", $linea);
-			print "$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+
+			if ($_[3] eq "y")
+			{
+				$infoSalida = $infoSalida."$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+			}
+			else
+			{
+				print "$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+			}
 		}
+	}
+
+	if ($_[3] eq "y")
+	{
+		print SALIDA $infoSalida;
+		close(SALIDA);
 	}
 }
 
+# ------------------------------------------------------------------
+# SUB, Muestra la info de Menor Precio (omite los Precios Cuidados).
+# ------------------------------------------------------------------
 sub MostrarMenuM_MenorPrecio
 {
+	system("clear");
+
 	my $idMenor = 0;
 	my $productoMenor = "";
 	my $superIdMenor = 0;
 	my $descripcionMenor = "";
 	my $precioMenor = 9999;
+
+	if ($_[3] eq "y")
+	{
+		open(SALIDA,">salida.txt");
+		print SALIDA "NRO de ITEM \| PRODUCTO PEDIDO \| PRODUCTO ENCONTRADO \| PRECIO \| NOMBRE_SUPER-PROVINCIA\n";
+	}
+	else
+	{
+		print "NRO de ITEM \| PRODUCTO PEDIDO \| PRODUCTO ENCONTRADO \| PRECIO \| NOMBRE_SUPER-PROVINCIA\n";
+	}
 
 	for (my $i=1; $i<=$_[2];$i++)
 	{
@@ -86,7 +227,15 @@ sub MostrarMenuM_MenorPrecio
 		{
 			if ($i != 1)
 			{
-				print "$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+				if ($_[3] eq "y")
+				{
+					$infoSalida = $infoSalida."$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+				}
+				else
+				{	
+					print "$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+				}
+				
 			}
 
 			$idMenor = 0;
@@ -111,14 +260,42 @@ sub MostrarMenuM_MenorPrecio
 
 			if ($i == $_[2])
 			{
-				print "$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+				if ($_[3] eq "y")
+				{
+					$infoSalida = $infoSalida."$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+				}
+				else
+				{
+					print "$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+				}
 			}
 		}
 	}
+
+	if ($_[3] eq "y")
+	{
+		print SALIDA $infoSalida;
+		close(SALIDA);
+	}
 }
 
+# ------------------------------------------
+# SUB, Muestra la info de precios faltantes.
+# ------------------------------------------
 sub MostrarMenuF_Faltante
 {
+	system("clear");
+
+	if ($_[3] eq "y")
+	{	
+		open(SALIDA,">salida.txt");
+		print SALIDA "NRO de ITEM \| PRODUCTO PEDIDO \| PRODUCTO ENCONTRADO \| PRECIO \| NOMBRE_SUPER-PROVINCIA\n";
+	}
+	else
+	{
+		print "NRO de ITEM \| PRODUCTO PEDIDO \| PRODUCTO ENCONTRADO \| PRECIO \| NOMBRE_SUPER-PROVINCIA\n";
+	}
+
 	for (my $i=1;$i<=$_[2];$i++)
 	{
 		my $linea = $_[0]{$i};
@@ -132,12 +309,32 @@ sub MostrarMenuF_Faltante
 		
 			if (length($arrayData[2]) > 0)
 			{	
-				print "$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+				if ($_[3] eq "y")
+				{
+					$infoSalida = $infoSalida."$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+				}
+				else
+				{
+					print "$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+				}
 			}
 			else
 			{
-				print "$arrayData[0] \| $arrayData[1] \| $arrayData[2] \| $arrayData[3] \| $arrayData[4]\n";
+				if ($_[3] eq "y")
+				{
+					$infoSalida = $infoSalida."$arrayData[0] \| $arrayData[1] \| $arrayData[2] \| $arrayData[3] \| $arrayData[4]\n";
+				}
+				else
+				{
+					print "$arrayData[0] \| $arrayData[1] \| $arrayData[2] \| $arrayData[3] \| $arrayData[4]\n";
+				}
 			}
 		}
+	}
+
+	if ($_[3] eq "y")
+	{
+		print SALIDA $infoSalida;
+		close(SALIDA);
 	}
 }
