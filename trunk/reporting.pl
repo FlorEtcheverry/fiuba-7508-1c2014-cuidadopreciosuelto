d2afd1c0217902a4a592189f7caa96372e1f55f8
ramirodoi <ramirodoi@7dd76c45-ab5f-dfa8-2945-61286fd544e2>
2014-04-29 02:05:04 +0000

    + Se agregan las listas de ejemplo para correr el reporting.pl * Se actualizó la versión de reporting.pl   Muestra:           ayuda           referencia           menor precio           faltante

--- trunk/reporting.pl
+++ trunk/reporting.pl
@@ -1,30 +1,40 @@
 #! /usr/bin/perl
 
-# $string1 = <STDIN>;
-# $string2 = <STDIN>;
-# chomp($string1);
-# chomp($string2);
-# print "usted escribio $string1 y ademas $string2\n";
-# ----------------------------------------------------
-
 open(LISTA_PRESUPUESTADA,"<pepe.xxx") || die "ERROR: el archivo de Lista Presupuestada no existe";
 open(SUPER, "SUPER.MAE") || die "ERROR: el archivo de SUPER.MAE no existe";
 
+# --------------------------------
+# Cargamos la Lista Presupuestada.
+# --------------------------------
 $cantDeLineas = 0;
-%hashListaPresupuestada = ($cantDeLineas,"Informacion");
+%hashListaPresupuestada = ($cantDeLineas, "Informacion");
 
 while ($lineaEntrada = <LISTA_PRESUPUESTADA>)
 {
-	# print $lineaEntrada
 	$cantDeLineas++;
 	$hashListaPresupuestada{$cantDeLineas} = $lineaEntrada;
 }
 
+# -----------------------------------
+# Cargamos la Lista de Supermercados.
+# -----------------------------------
+%hashSuper = (0, "Informacion", 1, "Precios Cuidados");
+
+while ($lineaEntrada = <SUPER>)
+{
+	@arrayData = split(";", $lineaEntrada);
+	$id = $arrayData[0];
+	$nombre_provincia = $arrayData[2]."-".$arrayData[1];
+	$hashSuper{$id} = $nombre_provincia;
+}
+
+
 # print "$hashListaPresupuestada{0}\n";
 # print "$hashListaPresupuestada{1}\n";
 # &MostrarMenuA_Ayuda;
-# &MostrarMenuF_Faltante(\%hashListaPresupuestada, $cantDeLineas);
-# &MostrarMenuR_Referencia(\%hashListaPresupuestada, $cantDeLineas);
+# &MostrarMenuF_Faltante(\%hashListaPresupuestada, \$hashSuper, $cantDeLineas);
+# &MostrarMenuR_Referencia(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas);
+&MostrarMenuM_MenorPrecio(\%hashListaPresupuestada, \%hashSuper, $cantDeLineas);
 
 close (LISTA_PRESUPUESTADA);
 close (SUPER);
@@ -44,29 +54,90 @@ sub MostrarMenuA_Ayuda
 
 sub MostrarMenuR_Referencia
 {
-	for($i=1;$i<=$_[1];$i++)
+	for (my $i=1;$i<=$_[2];$i++)
 	{
-		$linea = $_[0]{$i};
+		my $linea = $_[0]{$i};
 		chomp($linea);
 
 		if ($linea =~ /[^;]*;[^;]*;1;.*/)
 		{
-			print "$linea\n";
-		} 
+			my @arrayData = split(";", $linea);
+			print "$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+		}
+	}
+}
+
+sub MostrarMenuM_MenorPrecio
+{
+	my $idMenor = 0;
+	my $productoMenor = "";
+	my $superIdMenor = 0;
+	my $descripcionMenor = "";
+	my $precioMenor = 9999;
+
+	for (my $i=1; $i<=$_[2];$i++)
+	{
+		my $linea = $_[0]{$i};
+		chomp($linea);
+
+		@arrayData = split(";", $linea);
+
+		if ($linea =~ /[^;]*;[^;]*;1;.*/)
+		{
+			if ($i != 1)
+			{
+				print "$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+			}
+
+			$idMenor = 0;
+			$productoMenor = "";
+			$superIdMenor = 0;
+			$descripcionMenor = "";
+			$precioMenor = 9999;
+		}
+		else
+		{
+			if (length($arrayData[4]) > 0)
+			{
+				if ($arrayData[4] <= $precioMenor)
+				{
+					$idMenor = $arrayData[0];
+					$productoMenor = $arrayData[1];
+					$superIdMenor = $arrayData[2];
+					$descripcionMenor = $arrayData[3];
+					$precioMenor = $arrayData[4];
+				}
+			}
+
+			if ($i == $_[2])
+			{
+				print "$idMenor \| $productoMenor \| $descripcionMenor \| $precioMenor \| $_[1]{$superIdMenor}\n";
+			}
+		}
 	}
 }
 
 sub MostrarMenuF_Faltante
 {
-	for($i=1;$i<=$_[1];$i++)
+	for (my $i=1;$i<=$_[2];$i++)
 	{
-		$linea = $_[0]{$i};
+		my $linea = $_[0]{$i};
 		chomp($linea);
-		$chr = chop($linea);
+		my $chr = chop($linea);
 		
-		if($chr eq ';')
+		if ($chr eq ';')
 		{
-			print "$linea\n";
+			$linea = $linea.";";
+			my @arrayData = split(";",$linea);
+		
+			if (length($arrayData[2]) > 0)
+			{	
+				print "$arrayData[0] \| $arrayData[1] \| $arrayData[3] \| $arrayData[4] \| $_[1]{$arrayData[2]}\n";
+			}
+			else
+			{
+				print "$arrayData[0] \| $arrayData[1] \| $arrayData[2] \| $arrayData[3] \| $arrayData[4]\n";
+			}
 		}
 	}
 }
